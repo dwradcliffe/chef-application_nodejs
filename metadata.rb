0e82e1f2a0bb89c900dfb1ee8ea5011291c99623
David Radcliffe <radcliffe.david@gmail.com>
2013-10-18 13:17:08 -0400

    add npm dependency

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,9 @@ maintainer_email "radcliffe.david@gmail.com"
 license          "MIT"
 description      "Deploys and configures Node.js applications"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "application", "~> 3.0"
 depends "runit", "~> 1.0"
 depends "nodejs"
+depends "npm"
